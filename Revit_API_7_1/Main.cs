209454c989efb12da13d8067fbacb7c5026bfd3c
cadman77 <cadman77@yandex.ru>
2022-04-03 21:40:17 +0300

    Add project files.

---
+++ Revit_API_7_1/Main.cs
@@ -0,0 +1,34 @@
+﻿// Разработать WPF-приложение для генерации нескольких листов выбранного формата. 
+// Приложение работает следующим образом: 
+// Открывается окно со списком типов семейств «Основные надписи» («Title block»).
+// Ниже в поле вписывается количество создаваемых листов. 
+// В конце пользователь нажимает кнопку «Создать листы». 
+// В модели создаётся указанное количество листов определённого типа.
+//
+// Добавьте возможность выбора вида, который должен вставляться в каждый новый лист.
+// Добавьте возможность заполнения параметра «Designed By» из текстового поля для
+// всех создаваемых листов.
+
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revit_API_7_1
+{
+    [Transaction(TransactionMode.Manual)]
+
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            MainView mainView = new MainView(commandData);
+            mainView.ShowDialog();
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ Revit_API_7_1/MainViewViewModel.cs
@@ -0,0 +1,82 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Revit_API_7_1
+{
+    class MainViewViewModel
+    {
+        private ExternalCommandData _commandData;
+
+        private UIDocument uidoc;
+        private Document doc;
+
+        public List<FamilySymbol> ProjectTitleBlocks { get; } = new List<FamilySymbol>();
+        public FamilySymbol SelectedTitleBlock { get; set; }
+
+        public DelegateCommand CreateCommand { get; }
+
+        static readonly string posIntMask = @"^\d+$";
+        readonly Regex intRGX = new Regex(posIntMask);
+
+        private int sheetQTY;
+        public int SheetQTY
+        {
+            get => sheetQTY;
+            set
+            {
+                if (value != 0 && intRGX.IsMatch(value.ToString()))
+                {
+                    sheetQTY = value;
+                }
+            }
+        }
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            uidoc = _commandData.Application.ActiveUIDocument;
+            doc = uidoc.Document;
+
+            CreateCommand = new DelegateCommand(OnCreateCommand);
+
+            List<FamilySymbol> projectTitleBlocks = new FilteredElementCollector(doc)
+                .WhereElementIsElementType()
+                .OfCategory(BuiltInCategory.OST_TitleBlocks)
+                .Cast<FamilySymbol>()
+                .ToList(); // ???
+
+            ProjectTitleBlocks = projectTitleBlocks;
+
+        }
+        public EventHandler CloseRequest;
+
+        private void OnCreateCommand()
+        {
+            if (SelectedTitleBlock == null || sheetQTY < 1)
+                return;
+
+            using (Transaction ts = new Transaction(doc, "Sheet  Transaction"))
+            {
+                ts.Start();
+
+                for (int i = 0; i < sheetQTY; i++)
+                    ViewSheet.Create(doc, SelectedTitleBlock.Id);
+
+                ts.Commit();
+            }
+            RaiseCloseRequest();
+        }
+
+        public void RaiseCloseRequest()
+        {
+            CloseRequest?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
